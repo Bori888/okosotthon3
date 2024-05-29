8f5ad9b2afc5b7cb416af4c1eb42a3c9788bc33c
Bori888 <ilyes.bori8@gmail.com>
2024-05-29 15:16:53 +0200

    first commit

---
+++ Feladatok.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+class Feladatok():
+    def __init__(self):
+        self.ev3 = EV3Brick()# tégla
+        self.cs =ColorSensor(Port.S3)#szenzorok
+        self.ts =TouchSensor(Port.S1)
+        self.us =UltrasonicSensor(Port.S4)
+        
+    def okos_postalada(self):
+        #ha valaki ki nyitja a postaládát akor meg változik a szin és jelezni hogy levél érkezett
+        csukva_szin =self.cs.color()
+        if(self.cs.color()!=csukva_szin):
+            ev3.screen.print("Leveled érkezett!")
+            #szine nem ani mint amugy akkor kuldon uzenetet különben nem
+
+
+    def csipog(self):
+        self.ev3.speaker.beep()
+
+    
