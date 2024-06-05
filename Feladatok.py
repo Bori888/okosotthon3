558f1838d86fb2fc64f311c20c57d86f484d92e3
Bori888 <ilyes.bori8@gmail.com>
2024-06-05 12:52:58 +0200

    kész, minden müködik

--- Feladatok.py
+++ Feladatok.py
@@ -12,17 +12,18 @@ class Feladatok():
     def __init__(self):
         self.ev3 = EV3Brick()# tégla
         self.cs =ColorSensor(Port.S3)#szenzorok
-        self.ts =TouchSensor(Port.S1)
-        self.us =UltrasonicSensor(Port.S4)
+        #self.ts =TouchSensor(Port.S1)
+        #self.us =UltrasonicSensor(Port.S4)
         
     def okos_postalada(self):
         #ha valaki ki nyitja a postaládát akor meg változik a szin és jelezni hogy levél érkezett
         csukva_szin =self.cs.color()
+        wait(1000)
         if(self.cs.color()!=csukva_szin):
             wait(1000)
-            ev3.screen.print("Leveled érkezett!")
+            self.ev3.screen.print("Leveled érkezett!")
             #szine nem ani mint amugy akkor kuldon uzenetet különben nem
-
+        wait(1000)
 
     def csipog(self):
         self.ev3.speaker.beep()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
- import Feladatok
+import Feladatok
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
